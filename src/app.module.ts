d754b6dc4f62e51aa41e5b343e5f036da3a0104f
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-03 10:00:00 -0300

    Update Module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonsModule } from './persons/persons.module';
 
 @Module({
-    imports: [PersonsModule],
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'public'),
+        }),
+        PersonsModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
