f33a9ba7eb9d7939b1540bc0820e098444741eb6
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-04 23:02:36 -0300

    refacto mains.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common'; //lo agregamos para validar lo que agregamos en el dto
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { env } from 'process';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
