c35b4d6ea8c771dc60ab25425551f8c66905fd7c
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-04 23:02:12 -0300

    refactor controllers

--- src/persons/persons.module.ts
+++ src/persons/persons.module.ts
@@ -3,7 +3,7 @@ import { PersonsController } from './controllers/persons/persons.controller';
 import { PersonsService } from './services/persons/persons.service';
 
 @Module({
-  controllers: [PersonsController],
-  providers: [PersonsService]
+    controllers: [PersonsController],
+    providers: [PersonsService],
 })
 export class PersonsModule {}
