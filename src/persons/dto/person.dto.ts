6ceedbf460c98a87516c38ef55e845d58bbd72df
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-05 23:03:46 -0300

    Update person.dto

--- src/persons/dto/person.dto.ts
+++ src/persons/dto/person.dto.ts
@@ -1,4 +1,13 @@
-import { IsString, IsNotEmpty, IsEmail, Length } from 'class-validator';
+import {
+    IsString,
+    IsNotEmpty,
+    IsEmail,
+    Length,
+    IsOptional,
+    IsPositive,
+    Min,
+    ValidateIf,
+} from 'class-validator';
 import { PartialType, ApiProperty } from '@nestjs/swagger';
 
 export class CreatePersonDto {
@@ -14,3 +23,21 @@ export class CreatePersonDto {
 }
 
 export class UpdatePersonDto extends PartialType(CreatePersonDto) {}
+
+export class FilterDto {
+    @IsOptional()
+    @IsPositive()
+    limit: number;
+
+    @IsOptional()
+    @Min(0)
+    offset: number;
+
+    @IsOptional()
+    @IsPositive()
+    minPrice: number;
+
+    @ValidateIf((item) => item.minPrice)
+    @IsPositive()
+    maxPrice: number;
+}
