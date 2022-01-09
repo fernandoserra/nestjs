6ca918a907854dcc6d701d9a3735bca220f0a9e5
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-09 15:44:52 -0300

    Update controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
+
+    @Get()
+    redirect(@Res() res) {
+        return res.redirect('/public/index');
+    }
+
+    /*@Get()
+    @Render('public/index.html')
+    root() {
+        return {};
+    }*/
+
+    @Get()
+    getIndex(): string {
+        return 'hola';
+    }
 
     @Get()
     getHello(): string {
